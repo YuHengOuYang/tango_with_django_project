4b423f2e1ccfd9c85b92cad289a266ffdcd9820f
YuHeng OuYang <2598916o@student.gla.ac.uk>
2022-01-30 20:28:50 +0000

    third commit

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import rango
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #return HttpResponse("Rango says hey there partner!")
+    #return HttpResponse('<a href="http://127.0.0.1:8000/rango/about/">Rango says hey there partner!')
+    return HttpResponse("<a href='/rango/about/'>About</a> Rango says hey there partner!")
+def about(request):
+    #return HttpResponse("""<a href="http://127.0.0.1:8000/">HttpResponse: 'Rango says here is the about page.'""")
+    return HttpResponse("<a href='/rango/'>Index</a> Rango says here is the about page.")
