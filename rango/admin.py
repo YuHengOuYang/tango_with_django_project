3a2446a030d8e04a5deb3916c6160a0049336236
YuHeng OuYang <2598916o@student.gla.ac.uk>
2022-02-05 20:18:21 +0000

    Fifth—chapter

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category', 'url')
+
+
 # Register your models here.
+from rango.models import Category, Page
+admin.site.register(Category)
+#admin.site.register(Page)
+admin.site.register(Page,PageAdmin)
+#在page页面同时显示title，category和url
+
+
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. model其实就是ERD里面的entity
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views=models.IntegerField(default=0)
+    likes=models.IntegerField(default=0)
+    class Meta:
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)#因为指定的是外键所以不用说type
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    #变量名= 左边是fields 右边是types
+    def __str__(self):
+        return self.title
